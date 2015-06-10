012bf97af0a06b5dd2daffe878db987d4e1a1e87
Du Jingwu <djw@t-chip.com.cn>
2015-06-10 10:20:59 +0800

    add rknand support

--- scripts/init-premount/mtd-by-name.sh
+++ scripts/init-premount/mtd-by-name.sh
@@ -18,7 +18,7 @@ if [ -d /sys/class/mtd/ ]; then
 	    i=${i%/name}
 	    ln -s /dev/mtdblock${i} /dev/block/mtd/by-name/${name}
     done
-else
+elif [ -d /sys/block/mmcblk0/ ]; then
     # emmc
     for i in `ls /sys/block/mmcblk0/mmcblk0p*/volname`; do
 	    name=`cat ${i}`
@@ -26,7 +26,14 @@ else
 	    i=${i%/volname}
 	    ln -s /dev/${i} /dev/block/mtd/by-name/${name}
     done
+else
+    # /dev/rknand_*
+    for i in `ls /dev/rknand_* 2>/dev/null`; do
+        name=${i##/dev/rknand_}
+        ln -s ${i} /dev/block/mtd/by-name/${name}
+    done
 fi
+
 
 exit 0
 
